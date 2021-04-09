34e110acfdc0338a21ebdbb367b0c4e3512da938
uchokon <69696089+uchokon@users.noreply.github.com>
2021-04-09 19:07:18 -0400

    Folder fixes

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 import express from 'express';
 import db from './database/initializeDB.js';
 import apiRoutes from './routes/apiRoutes.js';
@@ -6,8 +7,11 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+const staticFolder = 'public';
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static(staticFolder));
 
 app.use('/api', apiRoutes);
 
