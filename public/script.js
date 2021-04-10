2f982056be7e93bd7767f0ea9848ef33b22c8626
uchokon <69696089+uchokon@users.noreply.github.com>
2021-04-09 21:04:45 -0400

    lab fixes

---
+++ public/script.js
@@ -0,0 +1,16 @@
+/* eslint-disable no-console */
+async function diningTable() {
+  const diningHalls = await fetch('/api/dining');
+  const dHjson = await diningHalls.json();
+  const resTable = document.querySelector('.tableContent');
+  console.log(resTable);
+  console.log(dHjson);
+
+  dHjson.data.forEach((item) => {
+    console.log(item);
+    const appendItem = document.createElement('tr');
+    appendItem.innerHTML = `<td>${item.hall_id}</td><td>${item.hall_name}</td><td>${item.hall_address}</td>`;
+    resTable.append(appendItem);
+  });
+}
+window.onload = diningTable;
