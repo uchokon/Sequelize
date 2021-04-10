b43cb20103427dc35122792511ef4d05ba5945bf
uchokon <69696089+uchokon@users.noreply.github.com>
2021-04-09 21:56:46 -0400

    week 10

--- public/script.js
+++ public/script.js
@@ -2,7 +2,7 @@
 async function diningTable() {
   const diningHalls = await fetch('/api/dining');
   const dHjson = await diningHalls.json();
-  const resTable = document.querySelector('.tableContent');
+  const resTable = document.querySelector('.tableConten');
   console.log(resTable);
   console.log(dHjson);
 
